7c3ea4c64fa2ca82fde7a616f011ea7e4f3f174d
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-20 18:53:39 +0200

    Install scrapy_djangoitem

--- src/federica_crawler/federica_crawler/settings.py
+++ src/federica_crawler/federica_crawler/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scrapy_djangoitem',
     'apps.crawler.apps.CrawlerConfig'
 ]
 
