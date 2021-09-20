263a2320d3822e6ffa165bba102eb46ae5e47a79
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-20 15:10:38 +0200

    Add Course model

---
+++ federica/federica/spiders/models.py
@@ -0,0 +1,10 @@
+class Course():
+
+    def __init__(self=None, title=None, link=None, area=None, status=None, teacher=None, short_description=None, description=None):
+        self.title = title
+        self.link = link
+        self.area = area
+        self.status = status
+        self.teacher = teacher
+        self.short_description = short_description
+        self.description = description
