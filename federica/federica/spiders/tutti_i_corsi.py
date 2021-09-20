7256f518870c7b10cfea3801d78dbd433574a2e3
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-20 15:29:08 +0200

    Parse description with parse details

--- federica/federica/spiders/tutti_i_corsi.py
+++ federica/federica/spiders/tutti_i_corsi.py
@@ -7,12 +7,12 @@ class TuttiICorsiSpider(scrapy.Spider):
     allowed_domains = ['federica.eu']
     start_urls = ['http://www.federica.eu/tutti-i-mooc/']
 
-    def parse_details(self, response):
+    def parse_details(self, response, course: Course):
         description_div = response.xpath(
             '//div[./*[contains(text(), "Descrizione")]]')
-        yield {
-            'description': ' '.join(description_div.css('p::text, strong::text').getall())
-        }
+        course.description = ' '.join(
+            description_div.css('p::text, strong::text').getall())
+        return course.__dict__
 
     def parse_base_info(self, response):
         for course in response.css('div.mooc-main-box')[0:5]:
@@ -22,7 +22,9 @@ class TuttiICorsiSpider(scrapy.Spider):
             link = titolo_box.css('a')
             if link:
                 out.title = link.css('::text').get()
-                out.link = link.attrib['href']
+                out.link = response.urljoin(link.attrib['href'])
+                details_request = scrapy.Request(
+                    out.link+'&lang=it', self.parse_details, cb_kwargs=dict(course=out))
             else:
                 out.title = titolo_box.css('::text').get()
                 out.link = None
@@ -33,7 +35,7 @@ class TuttiICorsiSpider(scrapy.Spider):
             out.teacher = course.css('div.docente-box::text').get()
             out.short_description = course.css('p.abstract-box::text').get()
 
-            yield out.__dict__
+            yield details_request
 
     def parse(self, response):
-        yield from self.parse_base_info(response)
+        return self.parse_base_info(response)
