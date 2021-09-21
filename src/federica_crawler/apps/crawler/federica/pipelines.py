47ad91bed323525c08517280749e71847287fe3e
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-21 11:55:20 +0200

    Add return to pipelines Add NotNullPipeline Add SaveItemPipeline

--- src/federica_crawler/apps/crawler/federica/pipelines.py
+++ src/federica_crawler/apps/crawler/federica/pipelines.py
@@ -13,9 +13,51 @@ from apps.crawler.federica.items import CourseItem
 class TitlePipeline:
     """Pipeline that ensure every item has a title"""
 
-    def process_item(self, item: CourseItem, spider):
+    def process_item(self, item: CourseItem, spider) -> CourseItem:
         """Check if item has a title field, in case it's blank or None drop the Item"""
         adapter = ItemAdapter(item)
         if not adapter.get('title'):
             raise DropItem(f"Missing title in {item}")
+
+        return item
+
+
+class NotNullPipeline:
+    """Pipline that ensure every None textbased field it's replaced by blank"""
+
+    def process_item(self, item: CourseItem, spider) -> CourseItem:
+        """Set None fields to a blank string
+
+        Work on the following fields:
+        url,
+        area,
+        status,
+        teacher,
+        short_description,
+        description
+        """
+
+        adapter = ItemAdapter(item)
+        if adapter.get('url') == None:
+            adapter['url'] = ''
+        if adapter.get('area') == None:
+            adapter['area'] = ''
+        if adapter.get('status') == None:
+            adapter['status'] = ''
+        if adapter.get('teacher') == None:
+            adapter['teacher'] = ''
+        if adapter.get('short_description') == None:
+            adapter['short_description'] = ''
+        if adapter.get('description') == None:
+            adapter['description'] = ''
+
+        return item
+
+
+class SaveItemPipeline:
+    """Pipeline that ensure there are no duplicates and saves Item to database"""
+
+    def process_item(self, item: CourseItem, spider) -> CourseItem:
+        """Check if Item it's already present in database, if not saves it"""
         item.save()
+        return item
--- src/federica_crawler/apps/crawler/federica/settings.py
+++ src/federica_crawler/apps/crawler/federica/settings.py
@@ -63,7 +63,9 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'apps.crawler.federica.pipelines.TitlePipeline': 300,
+   'federica.pipelines.TitlePipeline': 300,
+   'federica.pipelines.NotNullPipeline': 301,
+   'federica.pipelines.SaveItemPipeline': 302,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
