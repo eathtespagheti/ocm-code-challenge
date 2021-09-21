5a9de2c482c9351b747e8312c2005a6f12457658
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-21 18:19:28 +0200

    Add really basic views

--- src/federica_crawler/apps/courses/views.py
+++ src/federica_crawler/apps/courses/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Course
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'course/index.html'
+    context_object_name = 'courses'
+
+    def get_queryset(self):
+        """Return all Courses."""
+        return Course.objects.order_by('title')
+
+
+class DetailView(generic.DetailView):
+    model = Course
+    template_name = 'course/detail.html'
