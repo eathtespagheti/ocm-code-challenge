ae3475507879b456dfaf9cd6b0581167f9876843
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-21 11:40:43 +0200

    Disallow null fields in Course model, default to blank

--- src/federica_crawler/apps/crawler/models.py
+++ src/federica_crawler/apps/crawler/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
-    url = models.URLField(null=True)
-    area = models.CharField(max_length=200, null=True)
-    status = models.CharField(max_length=200, null=True)
-    teacher = models.CharField(max_length=200, null=True)
-    short_description = models.CharField(max_length=300, null=True)
-    description = models.CharField(max_length=1000, null=True)
+    url = models.URLField(blank=True, default='')
+    area = models.CharField(max_length=200, blank=True, default='')
+    status = models.CharField(max_length=200, blank=True, default='')
+    teacher = models.CharField(max_length=200, blank=True, default='')
+    short_description = models.CharField(max_length=300, blank=True, default='')
+    description = models.CharField(max_length=1000, blank=True, default='')
 
     def __str__(self) -> str:
         return self.title
