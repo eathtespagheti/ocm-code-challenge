b6802e44cbc1f720efa952212b46a9bd548fb748
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-21 16:45:17 +0200

    Add Teacher and Area models

--- src/federica_crawler/apps/courses/admin.py
+++ src/federica_crawler/apps/courses/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Course, Status
+from .models import Course, Status, Area, Teacher
 
 admin.site.register(Course)
 admin.site.register(Status)
+admin.site.register(Area)
+admin.site.register(Teacher)
--- src/federica_crawler/apps/courses/models.py
+++ src/federica_crawler/apps/courses/models.py
@@ -20,14 +20,48 @@ class Status(models.Model):
         return reverse("Status_detail", kwargs={"pk": self.pk})
 
 
+class Area(models.Model):
+    """This model represent a Course area"""
+    value = models.CharField(max_length=50, blank=True,
+                             default='', unique=True)
+
+    class Meta:
+        verbose_name = _("Area")
+        verbose_name_plural = _("Areas")
+
+    def __str__(self):
+        return self.value
+
+    def get_absolute_url(self):
+        return reverse("area_detail", kwargs={"pk": self.pk})
+
+
+class Teacher(models.Model):
+    """This model represent a Course teacher"""
+
+    name = models.CharField(max_length=100, blank=True,
+                            default='', unique=True)
+
+    class Meta:
+        verbose_name = _("Teacher")
+        verbose_name_plural = _("Teachers")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("teacher_detail", kwargs={"pk": self.pk})
+
+
 class Course(models.Model):
     """This model represent a single Course"""
 
     title = models.CharField(max_length=200, unique=True)
     url = models.URLField(blank=True, default='')
-    area = models.CharField(max_length=200, blank=True, default='')
+    area = models.ForeignKey('Area', on_delete=models.SET_NULL, null=True)
     status = models.ForeignKey('Status', on_delete=models.SET_NULL, null=True)
-    teacher = models.CharField(max_length=200, blank=True, default='')
+    teacher = models.ForeignKey(
+        'Teacher', on_delete=models.SET_NULL, null=True)
     short_description = models.CharField(
         max_length=300, blank=True, default='')
     description = models.CharField(max_length=1000, blank=True, default='')
