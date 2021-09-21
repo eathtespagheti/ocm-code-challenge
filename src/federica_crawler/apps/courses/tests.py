b00db1285cb0b7b9a58f459ff8828d0dfa4fc9e7
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-22 00:56:01 +0200

    Add tests for Status, Area, Teacher

--- src/federica_crawler/apps/courses/tests.py
+++ src/federica_crawler/apps/courses/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Status, Area, Teacher
 
-# Create your tests here.
+class StatusTestCase(TestCase):
+    def setUp(self):
+        Status.objects.create(value="test")
+
+    def test_has_value(self):
+        """Taste value field and __str__"""
+        s = Status.objects.get(value="test")
+        self.assertEqual(s.__str__(), 'test')
+
+class AreaTestCase(TestCase):
+    def setUp(self):
+        Area.objects.create(value="test")
+
+    def test_has_value(self):
+        """Taste value field and __str__"""
+        s = Area.objects.get(value="test")
+        self.assertEqual(s.__str__(), 'test')
+
+class TeacherTestCase(TestCase):
+    def setUp(self):
+        Teacher.objects.create(name="test")
+
+    def test_has_name(self):
+        """Taste name field and __str__"""
+        s = Teacher.objects.get(name="test")
+        self.assertEqual(s.__str__(), 'test')
