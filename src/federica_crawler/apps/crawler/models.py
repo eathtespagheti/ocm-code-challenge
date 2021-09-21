573108801bd8037a9d347ad6bd289fa08e3f910b
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-21 10:50:55 +0200

    Allow null fields to Course model Add __str__ function for Course model

--- src/federica_crawler/apps/crawler/models.py
+++ src/federica_crawler/apps/crawler/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
-    url = models.URLField()
-    area = models.CharField(max_length=200)
-    status = models.CharField(max_length=200)
-    teacher = models.CharField(max_length=200)
-    short_description = models.CharField(max_length=300)
-    description = models.CharField(max_length=1000)
+    url = models.URLField(null=True)
+    area = models.CharField(max_length=200, null=True)
+    status = models.CharField(max_length=200, null=True)
+    teacher = models.CharField(max_length=200, null=True)
+    short_description = models.CharField(max_length=300, null=True)
+    description = models.CharField(max_length=1000, null=True)
+
+    def __str__(self) -> str:
+        return self.title
