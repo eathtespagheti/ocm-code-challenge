8d73531f66d7d9c0c5bffe609ffae30b4b46272e
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-21 19:36:09 +0200

    Fix Reactor not restartable bug

--- src/federica_crawler/apps/crawler/run_crawler.py
+++ src/federica_crawler/apps/crawler/run_crawler.py
@@ -3,14 +3,31 @@ from scrapy.crawler import CrawlerRunner
 from apps.crawler.spiders.corsi import CorsiSpider
 from apps.crawler.settings import settings
 from twisted.internet import reactor
+from multiprocessing import Process, Queue
+
+# stolen from https://stackoverflow.com/questions/41495052/scrapy-reactor-not-restartable
+def run_spider(spider):
+    def f(q):
+        try:
+            runner = CrawlerRunner(settings=settings)
+            deferred = runner.crawl(spider)
+            deferred.addBoth(lambda _: reactor.stop())
+            reactor.run()
+            q.put(None)
+        except Exception as e:
+            q.put(e)
+
+    q = Queue()
+    p = Process(target=f, args=(q,))
+    p.start()
+    result = q.get()
+    p.join()
+
+    if result is not None:
+        raise result
 
 
 def collect_data():
     """Collect data using CorsiSpider and load it in the db"""
 
-    runner = CrawlerRunner(settings=settings)
-    runner.crawl(CorsiSpider)
-    d = runner.join()
-    d.addBoth(lambda _: reactor.stop())
-    reactor.run()
-
+    run_spider(CorsiSpider)
