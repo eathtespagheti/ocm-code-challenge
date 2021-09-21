0ea38b16b76ea4a9e0e211a84c40b5d7f3d54348
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-21 15:23:08 +0200

    Document Course model Add Meta in Course model

--- src/federica_crawler/apps/courses/models.py
+++ src/federica_crawler/apps/courses/models.py
@@ -1,13 +1,26 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext as _
+
 
 class Course(models.Model):
+    """This model represent a single Course"""
+
     title = models.CharField(max_length=200, unique=True)
     url = models.URLField(blank=True, default='')
     area = models.CharField(max_length=200, blank=True, default='')
     status = models.CharField(max_length=200, blank=True, default='')
     teacher = models.CharField(max_length=200, blank=True, default='')
-    short_description = models.CharField(max_length=300, blank=True, default='')
+    short_description = models.CharField(
+        max_length=300, blank=True, default='')
     description = models.CharField(max_length=1000, blank=True, default='')
 
-    def __str__(self) -> str:
+    class Meta:
+        verbose_name = _("Course")
+        verbose_name_plural = _("Courses")
+
+    def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("Course_detail", kwargs={"pk": self.pk})
